3e81af8c5523355ac1d5966f30e010f0368e9376
Praveen Nunna <praveen.nunna@hotmail.com>
2018-02-20 11:57:45 +0530

    Added Beer class and services

---
+++ src/app/Classes/Beer.ts
@@ -0,0 +1,16 @@
+export class Beer {
+
+  public id: string;
+
+  public name: string;
+
+  public description: string;
+
+  public alcoholPrecentage: string;
+
+  public styleName: string;
+
+  constructor() {
+  }
+
+}
